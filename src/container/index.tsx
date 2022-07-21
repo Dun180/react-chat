9842cdb10aa8d5efc6a66d880a0e2a9c0e1bd174
Dun180 <1213840551@qq.com>
2022-07-21 10:43:22 +0800

    first commit

---
+++ src/container/index.tsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react';
+import './index.scss'
+
+const App = () => {
+
+    return (
+        <div className="container">
+            <div className="main"></div>
+
+
+        </div>
+    )
+}
+export default App
