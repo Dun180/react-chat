de9523c929c4f680f0454f16cee09ae672844190
Dun180 <1213840551@qq.com>
2022-07-21 14:28:59 +0800

    feat:添加midway后端

---
+++ server/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1658384704130_9147',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
