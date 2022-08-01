d4eaa9aee4ec6ad590d6826265b8ceac7ac07a06
Dun180 <1213840551@qq.com>
2022-08-01 11:40:46 +0800

    fix:修改user

--- server/src/entity/user.ts
+++ server/src/entity/user.ts
@@ -1,18 +1,38 @@
 import { prop } from '@typegoose/typegoose'
 import { EntityModel} from "@midwayjs/typegoose";
+import mongoose from "mongoose";
 
 @EntityModel()
 export class User {
 
-  public _id?: string;
+  public _id: mongoose.Types.ObjectId;
 
-  @prop()
+  /**
+   * 用户名
+   */
+  @prop({
+    unique: true,
+    index: true,
+    required: true
+  })
   public username?: string;
 
-  @prop()
+  /**
+   * 密码
+   */
+  @prop({ required: true })
   public password?: string;
 
+  /**
+   * 名字
+   */
   @prop()
   public name?: string;
+
+  /**
+   * 创建时间
+   */
+  @prop({default: new Date().getTime()})
+  public createTime?: number;
 
 }
--- server/src/service/user.service.ts
+++ server/src/service/user.service.ts
@@ -1,5 +1,4 @@
 import { Provide } from '@midwayjs/decorator';
-import { IUserOptions } from '../interface';
 import {InjectEntityModel} from "@midwayjs/typegoose";
 import {ReturnModelType} from "@typegoose/typegoose";
 import { User } from '../entity/user';
@@ -10,22 +9,12 @@ export class UserService {
   @InjectEntityModel(User)
   userModel: ReturnModelType<typeof User>;
 
-  async getUser(options: IUserOptions) {
-    return {
-      uid: options.uid,
-      username: 'mockedName',
-      phone: '12345678901',
-      email: 'xxx.xxx@xxx.com',
-    };
-  }
+
   async login(username: string) {
     return await this.userModel.findOne({username:username}).exec();
   }
   async register(user: User) {
-    const session = await this.userModel.startSession()
-    await session.withTransaction(async () => {
 
-    })
     const findRes = await this.userModel.find({username:user.username}).exec();
     if (findRes.length === 0){
       const createRes = await this.userModel.create({
